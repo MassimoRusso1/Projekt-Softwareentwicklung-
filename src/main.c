5b69705c4d4822cb1db2672034ed64359216e91a
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-12 16:40:15 +0100

    push main programm

--- src/main.c
+++ src/main.c
@@ -1,14 +1,45 @@
 #include <stdio.h>
 
+int input=0;
+
 int main(void)
 {
-    printf("\033[4;92m========================\n");
-    printf("Choose your action: \n");
-    printf("List Developers \t[1]\n");
-    printf("Print Group Logo \t[2]\n");
-    printf("Print Group \t\t[3]\n");
-    printf("Exit \t\t\t[4]\n");
-    printf("========================\033[0m\n");
+while(1){
+printf("\033[4;92m========================\n");
+printf("Choose your action: \n");
+printf("List Developers \t[1]\n");
+printf("Print Group Logo \t[2]\n");
+printf("Print Group \t\t[3]\n");
+printf("Exit \t\t\t[4]\n");
+printf("========================\033[0m\n");
+printf("Enter your selection: ");
 
-    printf("Enter your selection: ");
+    scanf("%d", &input);
+
+    
+    
+    switch(input){
+
+    case 1:
+        printf("Eine 1 wurde gedrückt");
+    break;
+
+    case 2:
+        printf("Eine 2 wurde gedrückt");
+    break;
+
+    case 3:
+        printf("Eine 3 wurde gedrückt");
+    break;
+
+    case 4:
+     return 0;   
+    break;
+    
+    default:
+        printf("That was not a valid input!");
+    break;
+    }
+   }
+  return 0;
 }
