11b02f6d43fb03eb282daf75d2f84550b17fc153
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-12 17:40:45 +0100

    LIbary

---
+++ build/CMakeFiles/master.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/massi/Documents/Labor_Informatik/Projekt-Softwareentwicklung-/developer.c" "CMakeFiles/master.dir/developer.c.obj" "gcc" "CMakeFiles/master.dir/developer.c.obj.d"
+  "C:/Users/massi/Documents/Labor_Informatik/Projekt-Softwareentwicklung-/developer_group.c" "CMakeFiles/master.dir/developer_group.c.obj" "gcc" "CMakeFiles/master.dir/developer_group.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/master_exec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/master_exec.dir/main.c.obj"
+  "CMakeFiles/master_exec.dir/main.c.obj.d"
+  "libmaster_exec.dll.a"
+  "master_exec.exe"
+  "master_exec.exe.manifest"
+  "master_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/master_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
