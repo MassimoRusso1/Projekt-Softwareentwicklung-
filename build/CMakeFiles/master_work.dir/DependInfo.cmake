7d041dc7553818ccd5a981cbd7f1758b5a0f251c
Nico <122835168+tsarocin@users.noreply.github.com>
2023-03-12 18:04:40 +0000

    CMake made

---
+++ build/CMakeFiles/master_work.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/Projekt-Softwareentwicklung-/developer.c" "CMakeFiles/master_work.dir/developer.c.o" "gcc" "CMakeFiles/master_work.dir/developer.c.o.d"
+  "/workspaces/Projekt-Softwareentwicklung-/developer_group.c" "CMakeFiles/master_work.dir/developer_group.c.o" "gcc" "CMakeFiles/master_work.dir/developer_group.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/master_work.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/master_work.dir/developer.c.o"
+  "CMakeFiles/master_work.dir/developer.c.o.d"
+  "CMakeFiles/master_work.dir/developer_group.c.o"
+  "CMakeFiles/master_work.dir/developer_group.c.o.d"
+  "libmaster_work.pdb"
+  "libmaster_work.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/master_work.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
