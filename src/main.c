ad59437314e569745b658f2815cbbcea20afc521
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-13 21:23:19 +0000

    Second try to include header files result: done

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "developer.h"
+#include "developer_group.h"
 
 int main()
 {
-    int d1, d2;
+    int d1;
+    int d2;
 
     name_init(&d1, "Massimo Russo");
     name_init(&d2, "Nico Rast");
 
-    int a1, a2
+    int a1, a2;
 
     alias_init(&a1, "MassimoRusso1");
     alias_init(&a2, "tsarocin");
