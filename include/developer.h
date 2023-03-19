e529020905b2e84c93950b57fa1c0021d38b8cb7
Nico <122835168+tsarocin@users.noreply.github.com>
2023-03-19 19:46:57 +0000

    made the function developer, copying string in struct

--- include/developer.h
+++ include/developer.h
@@ -8,7 +8,7 @@ typedef struct
     char name[NAME_LENGTH];          
     char alias[NAME_LENGTH];
 }developer;
-void name_init(developer *developer_ptr,const char *name[max_length], const char *alias[max_length]);
+void name_init(developer *developer_ptr,const char *name[NAME_LENGTH], const char *alias[NAME_LENGTH]);
 void name_print(developer *developer_ptr);
 
 #endif
--- src/developer.c
+++ src/developer.c
@@ -3,7 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-void name_init(developer *const d, const char name[], const char alias[]){
-  strncpy(d->name, name, strlen(name) + 1);
-  strncpy(d->alias, alias, strlen(alias)+1);
+void name_init(developer *developer_ptr, const char *name[NAME_LENGTH], const char *alias[NAME_LENGTH])
+{
+  if (developer_ptr = NULL)
+  {
+    printf("\033[4;33mdeveloper init: invalid data\n\033[0m");        //error message if object is not valid
+  }
+  else
+  {
+    strncpy(developer_ptr->name, name, strlen(name) + 1);
+    strncpy(developer_ptr->alias, alias, strlen(alias) + 1);
+  }
 }
