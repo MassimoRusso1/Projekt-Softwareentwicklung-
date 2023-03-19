b9d900e5d5b68210fd2d6ddbeca7e71401c6f8fe
Nico <122835168+tsarocin@users.noreply.github.com>
2023-03-19 20:50:15 +0000

    function for name_print and implementation into the main programm

--- src/developer.c
+++ src/developer.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//This function initializes the "Developer" structure with the two parameters name and alias
 void name_init(developer *developer_ptr, const char *name[NAME_LENGTH], const char *alias[NAME_LENGTH])
 {
   if (developer_ptr = NULL)
@@ -11,7 +12,16 @@ void name_init(developer *developer_ptr, const char *name[NAME_LENGTH], const ch
   }
   else
   {
-    strncpy(developer_ptr->name, name, strlen(name) + 1);
+    strncpy(developer_ptr->name, name, strlen(name) + 1);             //string is copying into the struct, till the termination sign
     strncpy(developer_ptr->alias, alias, strlen(alias) + 1);
+  }
+}
+//This function prints the information from the "developer" structure to the screen. A pointer to a developer object is passed. 
+void name_print(developer *developer_ptr)
+{
+  printf("____________________________\n\n");
+  for (int i = 0; i <= 1; i++)
+  {
+    printf("**********************\n name: \033[4;35m%s\033[0m\n alias: %s\n **********************", developer_ptr[i].name, developer_ptr[i].alias);
   }
 }
--- src/main.c
+++ src/main.c
@@ -4,10 +4,31 @@
 #include "developer.h"
 #include "developer_group.h"
 
-int input=0;
 
 int main(void)
 {
+  int input = 0;
+  developer_group category;                                         //Initializes a variable of type developer_group.
+  developer Massimo, Nico;
+  
+
+  developer_group *categoryptr = &category;                         // Initializes a developer_group pointer and take it to the address of the category variable.
+  developer *devel = category.d;
+
+  developer *devel1 = categoryptr->d;
+  developer *devel2 = ++devel1;
+  devel1--;
+
+  name_init(devel1, "Massimo Russo", "MassimoRusso1");
+  name_init(devel2, "Nico Rast", "tsarocin");
+
+  developer *print_pointer[2] = {&Massimo, &Nico};
+  developer dl[2] = {Massimo, Nico};
+  developer *pointer_dl = &dl;
+
+  group_init(&category,"%s\n ***************************LOGO**********************************",&Nico);
+
+
 while(1){
 printf("\033[4;92m========================\n");
 printf("Choose your action: \n");
@@ -23,28 +44,29 @@ printf("Enter your selection: ");
     switch(input){
 
     case 1:
-        printf("The Developers of this Programm are:\n");
-        developer massimo, nico;
-        name_init(&massimo,"Massimo Russo","MassimoRusso1");
-        name_init(&nico, "Nico Rast","tsarocin");
-        printf("Our real Names are:\n");
-        printf("%s\n",massimo.name);
-        printf("%s\n",nico.name);
-        printf("Our nicknames are:\n");
-        printf("%s\n",massimo.alias);
-        printf("%s\n",nico.alias);
+        //printf("The Developers of this Programm are:\n");
+        //developer massimo, nico;
+        //name_init(&massimo,"Massimo Russo","MassimoRusso1");
+        //name_init(&nico, "Nico Rast","tsarocin");
+        //printf("Our real Names are:\n");
+        //printf("%s\n",massimo.name);
+        //printf("%s\n",nico.name);
+        //printf("Our nicknames are:\n");
+        //printf("%s\n",massimo.alias);
+        //printf("%s\n",nico.alias);
+        name_print(dl);
+        
     break;
 
     case 2:
-        printf("Here is our amazing Group Logo:");
-        developer_group logo;
-        logo_init(&logo,"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-        printf("%s",logo.logo);
+        logo_print(&category);
+
         
     break;
 
     case 3:
         printf("This is our group:");
+        developer_print(categoryptr);
     break;
 
     case 4:
