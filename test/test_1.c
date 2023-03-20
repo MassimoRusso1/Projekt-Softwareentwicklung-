ea95f5b144d0875e7ba10d74f89c970c95dd9899
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-20 09:27:07 +0000

    update

--- test/test_1.c
+++ test/test_1.c
@@ -1,17 +1,3 @@
-#ifndef DEVELOPER_GROUP_H_
-#define DEVELOPER_GROUP_H_
-
-#define NAME_LENGTH (2500)           //Erstellung eines Makros mit 25 Zeichen, nur positiv
-
-typedef struct 
-{
-    char logo[NAME_LENGTH]; 
-    developer d[2];         
-}developer_group;
-void group_init(developer_group *, const char *logo,const developer *d);
-void developer_print(developer_group *);
-void logo_print(developer_group *);
-#endif
 
 #ifndef DEVELOPER_H_
 #define DEVELOPER_H_
@@ -28,6 +14,21 @@ void name_print(developer *developer_ptr);
 
 #endif
 
+#ifndef DEVELOPER_GROUP_H_
+#define DEVELOPER_GROUP_H_
+
+#define MAX_LENGTH (50)           //Erstellung eines Makros mit 25 Zeichen, nur positiv
+
+typedef struct 
+{
+    char logo[MAX_LENGTH]; 
+    developer d[2];         
+}developer_group;
+void group_init(developer_group *, const char *logo,const developer *d);
+void developer_print(developer_group *);
+void logo_print(developer_group *);
+#endif
+
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -40,7 +41,6 @@ int main(void)
   
 
   developer_group *categoryptr = &category;                         // Initializes a developer_group pointer and take it to the address of the category variable.
-  developer *devel = category.d;
 
   developer *devel1 = categoryptr->d;
   developer *devel2 = ++devel1;
