f70263311e9ddf26543b00aa28a3f02431886925
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-20 10:27:47 +0000

    fixed spelling mistake

--- src/developer.c
+++ src/developer.c
@@ -4,14 +4,14 @@
 #include <string.h>
 
 //This function initializes the "Developer" structure with the two parameters name and alias
-void name_init(developer *d, char *name[], const char *alias[])
+void name_init(developer * const d, char name[], char alias[])
 {
     strncpy(d->name, name, strlen(name) + 1);             //string is copying into the struct, till the termination sign
     strncpy(d->alias, alias, strlen(alias) + 1);
   
 }
 //This function prints the information from the "developer" structure to the screen. A pointer to a developer object is passed. 
-void name_print(developer *d)
+void name_print(developer *const d)
 {
     printf("**********************\n name: %s\n alias: %s\n **********************", d->name.name, d->alias);
 }
