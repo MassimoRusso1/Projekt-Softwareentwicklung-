0ac6ab9bc7d4de8bfd2ce8b5becf28e92fbcce88
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-20 11:40:21 +0000

    one problem left

--- include/developer_group.h
+++ include/developer_group.h
@@ -2,14 +2,14 @@
 #define DEVELOPER_GROUP_H_
 #include "developer.h"
 
-#define NAME_LENGTH (2500)           //Erstellung eines Makros mit 25 Zeichen, nur positiv
+#define MAX_LENGTH (250)           //Erstellung eines Makros mit 25 Zeichen, nur positiv
 
 typedef struct 
 {
-    char logo[NAME_LENGTH]; 
+    char logo[MAX_LENGTH]; 
     developer list[2];         
 }developer_group;
 void group_init(developer_group *const d, char name1[],char alias1[], char name2[], char alias2[]);
 void group_print(developer_group *const d);
-void logo_print(developer_group *const d);
+void logo_print(developer_group * const d);
 #endif
