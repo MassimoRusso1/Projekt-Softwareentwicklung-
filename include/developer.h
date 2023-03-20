5b44debb9264037028a9f85ddd2762a639081276
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-20 10:04:45 +0000

    update developer.h

--- include/developer.h
+++ include/developer.h
@@ -8,7 +8,7 @@ typedef struct
     char name[NAME_LENGTH];          
     char alias[NAME_LENGTH];
 }developer;
-void name_init(developer *developer_ptr,const char *name[NAME_LENGTH], const char *alias[NAME_LENGTH]);
-void name_print(developer *developer_ptr);
+void name_init(developer *const d, char name[], char alias[]);
+void name_print(developer *const d);
 
 #endif
