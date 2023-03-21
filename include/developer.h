3c069461fb359558a105b61955aecf5b39588a48
Nico <122835168+tsarocin@users.noreply.github.com>
2023-03-21 20:59:36 +0000

    commits for the developer.h

--- include/developer.h
+++ include/developer.h
@@ -1,13 +1,16 @@
+//prevents multiple integration of the file
 #ifndef DEVELOPER_H_
 #define DEVELOPER_H_
 
-#define NAME_LENGTH (25U) // Erstellung eines Makros mit 25 Zeichen, nur positiv
-
+// Erstellung eines Makros mit 25 Zeichen, nur positiv
+#define NAME_LENGTH (25U)                                           
+//defines a data type "developer" with two structure variables "name" and "alias", both defined with the macro "NAME_LENGTH" to 25 characters
 typedef struct
 {
-    char name[NAME_LENGTH];
+    char name[NAME_LENGTH];                                         
     char alias[NAME_LENGTH];
 } developer;
+//two functions "name_init" and "name_print" are defined to initialize and print a developer's name and alias
 void name_init(developer *const d, char name[], char alias[]);
 void name_print(developer *const d);
 
