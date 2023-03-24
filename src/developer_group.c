08a03abfe2b19106ea7224e59f107d41d523f97c
MassimoRusso1 <122785115+MassimoRusso1@users.noreply.github.com>
2023-03-24 18:16:48 +0000

    added commits to developer group

--- src/developer_group.c
+++ src/developer_group.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// define a struct named grouplogo as a string
+// Define a struct named "grouplogo" as a string
 developer_group grouplogo = {.logo = {
                                  "\n"
                                  "88**Yb  dP*Yb  .dP*Y8 .dP*Y8\n"
@@ -11,28 +11,35 @@ developer_group grouplogo = {.logo = {
                                  "88**Yb Yb   dP o.`Y8b o.`Y8b\n"
                                  "88oodP  YbodP  8bodP' 8bodP'\n"}};
 
-// defines a function called "group_init" that initializes our team and takes parameters as pointer to developer group and four strings containing the names and aliases.
-// It also copies names and alias to a developer list.
+// Define a function called "logo_print" that takes a pointer to the "developer_group" struct.
+// This function prints the logo of the developer group to the console.
 void logo_print(developer_group *d)
 {
-  if (d == NULL)
+  if (d == NULL) // Pointer check
   {
-    printf("invalid value\n");
+    printf("invalid value\n"); // Print an error message 
   }
   else
   {
-    printf("\033[0:36m");
-    printf("%s\n", grouplogo.logo);
-    printf("\033[0m");
+    printf("\033[0:36m"); // Set the text color
+    printf("%s\n", grouplogo.logo); // Print the logo of the developer group
+    printf("\033[0m"); // Reset the text color
   }
 }
+
+// Define a function called "group_init" that initializes the developer group with the given names and aliases.
+// This function takes a pointer to the "developer_group" struct.
+// The function copies the names and aliases to the "list" array of the "developer_group" struct.
 void group_init(developer_group *const d, char dev1[], char dev1_alias[], char dev2[], char dev2_alias[])
 {
-  strncpy(d->list[0].name, dev1, strlen(dev1) + 1);
-  strncpy(d->list[0].alias, dev1, strlen(dev1_alias) + 1);
-  strncpy(d->list[1].name, dev2, strlen(dev2) + 1);
-  strncpy(d->list[1].alias, dev2, strlen(dev2_alias) + 1);
+  strncpy(d->list[0].name, dev1, strlen(dev1) + 1); // Copy the name of the first developer 
+  strncpy(d->list[0].alias, dev1, strlen(dev1_alias) + 1); // Copy the alias of the first developer 
+  strncpy(d->list[1].name, dev2, strlen(dev2) + 1); // Copy the name of the second developer 
+  strncpy(d->list[1].alias, dev2, strlen(dev2_alias) + 1); // Copy the alias of the second developer
 }
+
+// Define a function called "group_print" that prints the names and aliases of the developers.
+// This function takes a pointer to the "developer_group" struct.
 void group_print(developer_group *const d)
 {
   printf("************************\n Name: %s\n Alias: %s\n Name: %s\n Alias: %s\n************************\n", d->list[0].name, d->list[0].alias, d->list[1].name, d->list[1].alias);
